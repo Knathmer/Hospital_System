fb0b95b352bdeb6ad53813b8356b4a73c3412fc5
Eraj <anwaraeraj@gmail.com>
2024-09-11 19:34:42 -0500

    .

--- NodeBackend/server.js
+++ NodeBackend/server.js
@@ -1,11 +1,15 @@
-const http = require('http'); //Assigns alias to http package
-const fs = require('fs');
+// JAVASCRIPT SUCKS USE / FOR PATHS NOT \ WHICH IS THE DEFAULT FOR VSCODE
+
+const http = require('node:http');
+const fs = require('node:fs');
+
+
 
 
 
 const server = http.createServer((request, response) => { //listens
 
-    fs.readFile("test.html", (error, data) => {
+    fs.readFile('website.html', (error, data) => {
         if(error) {
             response.writeHead(500, { 'Content-Type': 'text/plain'});
             response.end("ERROR READING");
@@ -21,3 +25,6 @@ const server = http.createServer((request, response) => { //listens
 server.listen(3000, () => {
     console.log("Bababooey");
 })
+
+
+
---
+++ website.js
@@ -0,0 +1,13 @@
+console.log('test');
+
+
+
+/*
+
+const button = document.querySelector('.my_button'); //grabs the specific button and stores into variable
+
+button.addEventListener('click', () => { // anonymoose function
+    console.log('Button was clicked!');
+});
+
+*/
