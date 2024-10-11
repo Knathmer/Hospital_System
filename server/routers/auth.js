b284ab4523b5f98f8fc9f4c7d82ddf316248d4e4
Eraj Anwar <103916780+Knathmer@users.noreply.github.com>
2024-10-11 13:30:04 -0500

    Update auth.js
    Made compatible with ES modules

--- server/routers/auth.js
+++ server/routers/auth.js
@@ -1,10 +1,8 @@
 import express from 'express';
-import authController from "../controllers/authController";
+import authController from "../controllers/authController.js"; // .js needs to be added for ES modules
 const router = express.Router();
 
-router.post('/login', authController.login); //For when a log in request is sent from front end
-
+router.post('/login', authController.login);
 router.post('/register', authController.register);
 
-module.exports = router;
-
+export default router; //standard form for ES module exports
