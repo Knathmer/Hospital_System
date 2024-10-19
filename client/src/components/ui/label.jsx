1a1ed40eddbb96d29105fd77aea67329a0d176c5
Adam Johnson <adamgj3030@gmail.com>
2024-10-19 18:12:33 -0500

    solved merge conflicts with main

---
+++ client/src/components/ui/label.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Label = React.forwardRef(({ className = "", children, ...props }, ref) => {
+  return (
+    <label
+      ref={ref}
+      className={`text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 ${className}`}
+      {...props}
+    >
+      {children}
+    </label>
+  )
+})
+
+Label.displayName = "Label"
+
+export default Label
