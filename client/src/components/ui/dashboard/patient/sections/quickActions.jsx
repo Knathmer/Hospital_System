3bf9c62926a2730a03ae49c2a48f68d282e45200
Sthiber <sthiber133guevara@gmail.com>
2024-10-20 01:04:31 -0500

    Updated the quick action, when the button is clicked it takes you to patient/prescription

--- client/src/components/ui/dashboard/patient/sections/quickActions.jsx
+++ client/src/components/ui/dashboard/patient/sections/quickActions.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import Button from "../../../button.jsx";
+import { Navigate, useNavigate } from "react-router-dom";
 
 const QuickActions = () => {
+  const navigate = useNavigate(); //Initialize the navigate function.
+
+  const handlePrescription = () => {
+    console.log("Navigating to /patient/prescription"); //Debug log
+    navigate("/patient/prescription"); //Navigates to the /prescription page.
+  };
   return (
     <section className="mt-12">
       <h2 className="text-2xl font-bold mb-4">Quick Actions</h2>
       <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
-        <Button className="bg-pink-500 text-white hover:bg-pink-600">
+        <Button
+          to="/patient/prescription"
+          className="bg-pink-500 text-white hover:bg-pink-600"
+        >
           Request Prescription Refill
         </Button>
         <Button variant="outline">Message Your Doctor</Button>
