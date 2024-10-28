f58d0767e7c8b9702ad2a51c74afee4c362b6df9
Sthiber <sthiber133guevara@gmail.com>
2024-10-28 02:58:12 -0500

    Updated query to get data from the pharmacy.

--- server/queries/constants/selectQueries.js
+++ server/queries/constants/selectQueries.js
@@ -14,9 +14,11 @@ export const SELECT_ADDRESSID_QUERY = `
         `;
 
 //Patient Medication Queries
-export const SELECT_PATIENT_MEDICATION_INFORMATION_QUERY = `SELECT p.medicationName, p.dosage, p.frequency, p.start, 
-                                                            p.quantity, p.daySupply, p.instruction, d.lastName, d.firstName
+export const SELECT_PATIENT_MEDICATION_INFORMATION_QUERY = `SELECT p.medicationName, p.dosage, p.frequency, p.start,
+                                                            p.quantity, p.daySupply, p.instruction, d.lastName, d.firstName,
+                                                            ph.pharmacyName, ph.address, ph.city, ph.state, ph.zipCode, ph.phoneNumber
                                                             FROM prescription AS p
                                                             JOIN patient AS q ON p.patientID = q.patientID
                                                             JOIN doctor AS d ON p.doctorID = d.doctorID
+                                                            JOIN pharmacy AS ph ON q.pharmacyID = ph.pharmacyID
                                                             WHERE p.patientID = ?;`;
