b24983bd9795c8d791c0912629c7a3fd8131af61
issantidote <isabellatruong3@gmail.com>
2024-11-16 17:16:45 -0600

    "router bug"

--- client/src/routers/AdminRoutes.jsx
+++ client/src/routers/AdminRoutes.jsx
@@ -3,8 +3,6 @@ import { Routes, Route } from "react-router-dom";
 import AdminSidebar from "../components/userLoginRoot/AdminSidebar";
 import ManagePatients from "../components/users/admin/sections/ManagePatients";
 import ManageDoctors from "../components/users/admin/sections/ManageDoctors";
-import Settings from "../components/users/admin/sections/Settings";
-import SystemReports from "../components/users/admin/sections/SystemReports";
 
 const AdminRoutes = () => {
   return (
@@ -12,8 +10,6 @@ const AdminRoutes = () => {
       <Route path="dashboard" element={<AdminSidebar />} />
       <Route path="manage-patients" element={<ManagePatients />} />
       <Route path="manage-doctors" element={<ManageDoctors />} />
-      <Route path="system-reports" element={<SystemReports />} />
-      <Route path="settings" element={<Settings />} />
       {/* Add other admin routes here */}
     </Routes>
   );
